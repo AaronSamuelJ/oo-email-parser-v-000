f9e110dc82119df99d19ae7d2585e95dc1c0e5e9
Aaron Samuel <aasamuelj@gmail.com>
2017-09-30 10:09:20 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,10 +1,12 @@
 require 'pry'
-class EmailParser
+class EmailParser  "avi@test.com, arel@test.com" "avi@test.com arel@test.com"
   attr_accessor :emails
   def initialize(emails)
     @emails = emails
   end
   def parse
-    @emails.split(/[, ](?=[\w@.])/)
+    @emails.split(/[, ]/).select{|email| email[-3..-1] == "com"}
+    #a.split(/[, ]/).select{|email| email[-3..-1] == "com"}
+
   end #{}/\.(?=[\w])/
 end
